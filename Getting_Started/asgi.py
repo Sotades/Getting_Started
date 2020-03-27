1de7a71269c011d6c19a332db02abbb21171cb8c
Sotades <anthonyjbateman@gmail.com>
2020-03-27 10:42:00 +0200

    Initial commit

---
+++ Getting_Started/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Getting_Started project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Getting_Started.settings')
+
+application = get_asgi_application()
